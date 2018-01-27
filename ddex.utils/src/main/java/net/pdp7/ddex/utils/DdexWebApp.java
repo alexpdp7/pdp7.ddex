010f11e240053881da73fd083e08799ecb31e0fb
alex <alex@pdp7.net>
2018-01-27 13:28:16 +0100

    Heroku crap

---
+++ ddex.utils/src/main/java/net/pdp7/ddex/utils/DdexWebApp.java
@@ -0,0 +1,12 @@
+package net.pdp7.ddex.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdexWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdexWebApp.class, args);
+	}
+}
