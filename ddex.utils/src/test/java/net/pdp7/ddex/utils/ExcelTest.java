0f468aafff153ccbb497aee82275bd25ca4e3ff4
alex <alex@pdp7.net>
2018-01-27 12:59:46 +0100

    Fix warning in test

--- ddex.utils/src/test/java/net/pdp7/ddex/utils/ExcelTest.java
+++ ddex.utils/src/test/java/net/pdp7/ddex/utils/ExcelTest.java
@@ -10,10 +10,11 @@ import junit.framework.TestCase;
 
 public class ExcelTest extends TestCase {
 	public void test() throws Exception {
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet("foo");
-		HSSFRow row = sheet.createRow(0);
-		row.createCell(0).setCellValue("bar");
-		workbook.write(new File("test.xls"));
+		try(HSSFWorkbook workbook = new HSSFWorkbook()) {
+			HSSFSheet sheet = workbook.createSheet("foo");
+			HSSFRow row = sheet.createRow(0);
+			row.createCell(0).setCellValue("bar");
+			workbook.write(new File("test.xls"));
+		}
 	}
 }
