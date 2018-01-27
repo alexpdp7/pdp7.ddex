1333cdaf1fb4ba77e7a405894bf572bc122a7242
alex <alex@pdp7.net>
2018-01-27 12:47:15 +0100

    Flesh out DdexToTableConverter
    Move to Java 8 for lambdas and stuff

--- ddex.utils/src/main/java/net/pdp7/ddex/utils/DdexToTableConverter.java
+++ ddex.utils/src/main/java/net/pdp7/ddex/utils/DdexToTableConverter.java
@@ -1,26 +1,103 @@
 package net.pdp7.ddex.utils;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import net.ddex.xml.avs.avs.ReleaseType;
 import net.pdp7.ddex.utils.jaxb.NewReleaseMessage;
+import net.pdp7.ddex.utils.jaxb.Release;
+import net.pdp7.ddex.utils.jaxb.ReleaseId;
+import net.pdp7.ddex.utils.jaxb.SubTitle;
 
 public class DdexToTableConverter {
 
-	public Map<String, Object> convert(File file) throws JAXBException {
-		return convert((NewReleaseMessage) createUnmarshaller().unmarshal(file));
+	public Stream<Map<String, Object>> convert(File file) {
+		try {
+			return convert((NewReleaseMessage) createUnmarshaller().unmarshal(file));
+		} catch (JAXBException e) {
+			throw new DdexToTableConverterException.XmlProblem(file, e);
+		}
 	}
 
-	public Map<String, Object> convert(NewReleaseMessage release) {
-		return null;
+	public Stream<Map<String, Object>> convert(NewReleaseMessage newReleaseMessage) {
+		Release parentRelease = findParentRelease(newReleaseMessage);
+		Map<String, Object> parentReleaseColumns = getParentReleaseColumns(parentRelease);
+		return findReleasesOfType(newReleaseMessage, ReleaseType.TRACK_RELEASE)
+			.map(this::getTrackColumns)
+			.map((trackColumns) -> { trackColumns.putAll(parentReleaseColumns); return trackColumns; });
+	}
+
+	protected Map<String, Object> getParentReleaseColumns(Release parentRelease) {
+		Map<String, Object> parentReleaseColumns = new HashMap<String, Object>();
+		ReleaseId releaseId = parentRelease.getReleaseId().get(0);
+		parentReleaseColumns.put("GRID", releaseId.getGRid());
+		parentReleaseColumns.put("EAN", releaseId.getICPN().getValue());
+		parentReleaseColumns.put("Catalog Number", releaseId.getCatalogNumber().getValue());
+		parentReleaseColumns.put("Release Title", parentRelease.getReferenceTitle().getTitleText().getValue());
+		return parentReleaseColumns;
+	}
+
+	protected Map<String, Object> getTrackColumns(Release track) {
+		Map<String, Object> trackColumns = new HashMap<String, Object>();
+		trackColumns.put("ISRC", track.getReleaseId().get(0).getISRC());
+		trackColumns.put("Track Title", track.getReferenceTitle().getTitleText().getValue());
+		trackColumns.put("Track Subtitle", Optional.ofNullable(track.getReferenceTitle().getSubTitle()).map(SubTitle::getValue).orElse(""));
+		return trackColumns;
+	}
+	
+	protected Release findParentRelease(NewReleaseMessage newReleaseMessage) {
+		return findReleasesOfType(newReleaseMessage, ReleaseType.ALBUM)
+				.reduce((a, b) -> { throw new DdexToTableConverterException.MultipleParentReleasesFound(newReleaseMessage); })
+				.get();
+	}
+
+	protected Stream<Release> findReleasesOfType(NewReleaseMessage newReleaseMessage, ReleaseType releaseType) {
+		return newReleaseMessage
+				.getReleaseList()
+				.getRelease()
+				.stream()
+				.filter((Release r) -> r.getReleaseType().get(0).getValue() == releaseType);
 	}
 
 	protected Unmarshaller createUnmarshaller() throws JAXBException {
 		return JAXBContext.newInstance(NewReleaseMessage.class).createUnmarshaller();
 	}
 
+	public static class DdexToTableConverterException extends RuntimeException {
+		public DdexToTableConverterException(String reason, Throwable throwable) {
+			super(reason, throwable);
+		}
+
+		public DdexToTableConverterException(String reason) {
+			super(reason);
+		}
+
+		public static class MultipleParentReleasesFound extends DdexToTableConverterException {
+			public final NewReleaseMessage newReleaseMessage;
+
+			protected MultipleParentReleasesFound(NewReleaseMessage newReleaseMessage) {
+				super("Multiple parent releases found in " + newReleaseMessage);
+				this.newReleaseMessage = newReleaseMessage;
+			}
+		}
+
+		public static class XmlProblem extends DdexToTableConverterException {
+			public final File file;
+			public final JAXBException exception;
+
+			public XmlProblem(File file, JAXBException e) {
+				super("XML problem with " + file, e);
+				this.file = file;
+				this.exception = e;
+			}
+
+		}
+	}
 }
--- ddex.utils/src/test/java/net/pdp7/ddex/utils/DdexToTableConverterTest.java
+++ ddex.utils/src/test/java/net/pdp7/ddex/utils/DdexToTableConverterTest.java
@@ -1,13 +1,15 @@
 package net.pdp7.ddex.utils;
 
 import java.io.File;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import junit.framework.TestCase;
 
 public class DdexToTableConverterTest extends TestCase {
 	public void test() throws Exception {
-		Map<String, Object> output = new DdexToTableConverter().convert(new File("src/test/resources/8421597103035.xml"));
+		List<Map<String, Object>> output = new DdexToTableConverter().convert(new File("src/test/resources/8421597103035.xml")).collect(Collectors.toList());
 		System.out.println(output);
 	}
 }
