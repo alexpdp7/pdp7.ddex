4bd42512f14c7c2304e2718501ba1b9e08219b41
alex <alex@pdp7.net>
2018-02-06 21:06:09 +0100

    Clean imports

--- ddex.utils/src/main/java/net/pdp7/ddex/utils/DdexZipConverter.java
+++ ddex.utils/src/main/java/net/pdp7/ddex/utils/DdexZipConverter.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.input.CloseShieldInputStream;
